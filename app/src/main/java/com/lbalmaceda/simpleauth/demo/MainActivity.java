bb1169d58fc335ed5bb61adddd3b6b8ad4fe5ff8
lbalmaceda <balmacedaluciano@gmail.com>
2015-12-15 10:22:02 -0300

    add layout for the demo MainActivity. Add ResultActivity for showing the token.

--- app/src/main/java/com/lbalmaceda/simpleauth/demo/MainActivity.java
+++ app/src/main/java/com/lbalmaceda/simpleauth/demo/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.lbalmaceda.simpleauth.AuthMode;
@@ -21,26 +25,69 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent i = new Intent(MainActivity.this, SimpleAuthActivity.class);
-        i.putExtra(SimpleAuthActivity.KEY_AUTH_DOMAIN, Constants.AUTH_DOMAIN);
-        i.putExtra(SimpleAuthActivity.KEY_AUTH_CLIENT_ID, Constants.AUTH_CLIENT_ID);
-        i.putExtra(SimpleAuthActivity.KEY_AUTH_MODE, AuthMode.BOTH);
-        startActivityForResult(i, LOGIN_RESULT);
+        setContentView(R.layout.main_activity);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.login_mode_spinner);
+        final String[] items = getResources().getStringArray(R.array.login_modes);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, items);
+        spinner.setAdapter(adapter);
+
+        Button loginBtn = (Button) findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pos = spinner.getSelectedItemPosition();
+                switch (pos) {
+                    case 0:
+                        beginLoginFlow(AuthMode.EMAIL);
+                        break;
+                    case 1:
+                        beginLoginFlow(AuthMode.SOCIAL);
+                        break;
+                    case 2:
+                        beginLoginFlow(AuthMode.BOTH);
+                        break;
+                }
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == LOGIN_RESULT) {
             if (resultCode == RESULT_OK) {
-                Toast.makeText(MainActivity.this, "Login success", Toast.LENGTH_SHORT).show();
                 String token = data.getStringExtra(SimpleAuthActivity.EXTRA_TOKEN);
                 Log.d(TAG, String.format("Received token: %s", token));
+                showResult(token);
             } else {
-                Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Login canceled", Toast.LENGTH_SHORT).show();
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
-            finish();
+    }
+
+    /**
+     * Starts the login activity with SimpleAuth library
+     *
+     * @param mode a valid SimpleAuth AuthMode
+     */
+    private void beginLoginFlow(AuthMode mode) {
+        Intent i = new Intent(MainActivity.this, SimpleAuthActivity.class);
+        i.putExtra(SimpleAuthActivity.KEY_AUTH_DOMAIN, Constants.AUTH_DOMAIN);
+        i.putExtra(SimpleAuthActivity.KEY_AUTH_CLIENT_ID, Constants.AUTH_CLIENT_ID);
+        i.putExtra(SimpleAuthActivity.KEY_AUTH_MODE, mode);
+        startActivityForResult(i, LOGIN_RESULT);
+    }
+
+    /**
+     * Shows the auth token in a new activity
+     *
+     * @param authToken the logged in user auth token.
+     */
+    private void showResult(String authToken) {
+        Intent i = new Intent(MainActivity.this, ResultsActivity.class);
+        i.putExtra(ResultsActivity.KEY_TOKEN, authToken);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/lbalmaceda/simpleauth/demo/ResultsActivity.java
@@ -0,0 +1,30 @@
+package com.lbalmaceda.simpleauth.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+/**
+ * Created by lbalmaceda on 12/15/15.
+ */
+public class ResultsActivity extends AppCompatActivity {
+
+    private static final String TAG = ResultsActivity.class.getSimpleName();
+    public static final String KEY_TOKEN = "key_token";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results_activity);
+
+        Bundle extras = getIntent().getExtras();
+        String token = "error";
+        if (extras != null) {
+            token = extras.getString(KEY_TOKEN);
+        }
+
+        EditText resultInput = (EditText) findViewById(R.id.result_input);
+        resultInput.setText(token);
+    }
+}
